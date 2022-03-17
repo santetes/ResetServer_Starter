7bd1ea7daba5eaef17a6dc0af675890cf5be0cfe
Santos <sanmartgon@gmail.com>
2022-03-17 06:02:43 +0100

    fin integración tabla

--- public/index.js
+++ public/index.js
@@ -1,14 +1,33 @@
+import { gestionBorradoCelda, gestionSeleccionFila } from './helpers/funciones.js'
+
 // Elementos HTML
 const $btn_incluir = document.getElementById('incluir')
 const $resultado = document.getElementsByClassName('base')
-const $listado = document.getElementById('listado')
 const $limpiar = document.getElementById('clear')
+const $td_collection = document.querySelectorAll('td')
+const $fila_collection = document.querySelectorAll('.fila')
+const $btn_borrar = document.getElementById('btn-borrar')
 
-$btn_incluir.addEventListener('click', () => {
-    const resultado_txt = $resultado[0].innerText
-    const elemento_listado = document.createElement('li')
-    elemento_listado.classList.add('list-group-item')
-    elemento_listado.textContent = resultado_txt
-    $listado.appendChild(elemento_listado)
+$btn_incluir.addEventListener('click', (event) => {
+    event.preventDefault()
+    const textoEntrada = $resultado[0].innerText
+    $resultado.value = ''
+    const td_seleccionados = document.querySelectorAll('.table-primary')
+
+    try {
+        td_seleccionados.forEach((td) => {
+            if (td.textContent == '') {
+                td.textContent = textoEntrada
+                throw 'Encontrado hueco'
+            }
+        })
+    } catch (error) {
+        console.log(error)
+    }
+
     $limpiar.click()
 })
+
+gestionSeleccionFila($fila_collection, $td_collection, $btn_borrar)
+
+gestionBorradoCelda($td_collection, $btn_borrar)
